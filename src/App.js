934cfa4a7f4e458accf1622e73107bdefc087c7a
hyunju0824 <hyunjuhj0824@gmail.com>
2023-12-29 08:04:39 +0900

    긱사에서 노트북으로

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import MyJson from './myJson.json';
+// 유효성 검사 구성 요소
 
 function App() {
   return (
+    // App.css 파일에서 .App 클래스 스타일 사용
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Homework</h1>
+      {/* 매핑 */}
+      {
+      MyJson.map(myJson => {
+        return (
+          <div className="box" key={myJson.id}> 
+            {/* 제목만 표시 */}
+            <strong>{myJson.userId}</strong>
+            <strong>{myJson.userId}</strong>
+            {/* {myJson.title} */}
+          </div>
+        )
+      })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
