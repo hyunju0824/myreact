d3b8918b3596bd9e208556f62c15aa57d0b8fc35
hyunju0824 <hyunjuhj0824@gmail.com>
2024-01-14 18:09:52 +0900

    댓글 사진

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { atom, RecoilState, RecoilValue, useRecoilState, useRecoilValue } from 'recoil';
 import { myCommentList, myCommentUpdate } from '../recoil/atoms/myAtom';
 import Details from './Details';
+import logo from '../logo.png';
   
   export default function CommentList({postId}) {
      const [comments, setComments] = useRecoilState(myCommentList);
@@ -49,7 +50,7 @@ import Details from './Details';
               ? comments[postId].map((item) => (
             <li key={item.id} className="flex items-center justify-between gap-x-6 py-5">
               <div className="flex min-w-0 gap-x-4">
-                <img className="h-12 w-12 flex-none rounded-full bg-gray-50" alt="" />
+                <img className="h-12 w-12 flex-none rounded-full" alt="" src={logo} />
                 <div className="min-w-0 flex-auto">
                   <p className="text-sm font-semibold leading-6 text-gray-900">{item.name}</p>
                   <p className="mt-1 truncate text-xs leading-5 text-gray-500">{item.body}</p>
